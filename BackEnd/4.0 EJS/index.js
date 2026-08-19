93bd4d057793952e7cd01e723a89933b3c5250f9
Buntai-Fries <anuphmm2005@gmail.com>
2026-08-19 11:32:41 +0530

    EJS exercise.
    Week advisor.

--- BackEnd/4.0 EJS/index.js
+++ BackEnd/4.0 EJS/index.js
@@ -1,26 +1,28 @@
 import express from "express";
 import ejs from "ejs";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const port = 5000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const _day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const today = new Date();
+const __day = _day[today.getDay()];
+let _activity =
+  __day == "Sunday" || __day == "Saturday" ? "have fun" : "work hard";
+const data = { day: __day, activity: _activity };
 
 // app.set("view engine", "ejs"); (optional: connect ejs to express)
 
 app.get("/", (req, res) => {
-  const data = {
-    name: "Lenovo",
-  };
   res.render("index.ejs", data);
 });
-
-/*
-app.post("/submit", (req, res) => {
-  res.render("index.ejs", { name: req.body["name"] });
-});
-*/
 
 app.listen(port, () => {
   console.log(`The server port ${port} is up!`);
