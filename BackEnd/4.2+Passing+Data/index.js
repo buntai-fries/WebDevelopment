7decca1c6d415c55ac4647cd969f7860981ccbb2
Buntai-Fries <anuphmm2005@gmail.com>
2026-08-22 11:42:27 +0530

    A better solution for previous section
    Previous - Passing data to EJS Template.

--- BackEnd/4.2+Passing+Data/index.js
+++ BackEnd/4.2+Passing+Data/index.js
@@ -7,16 +7,12 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  let data = { form: "Enter your name below 👇" };
-  res.render("index.ejs", data);
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  const _fName = req.body["fName"];
-  const _lName = req.body["lName"];
-  const wordCount = _fName.length + _lName.length;
-  let data = { form: "There are " + wordCount + " letter in your name." };
-  res.render("index.ejs", data);
+  const wordCount = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numCount: wordCount });
 });
 
 app.listen(port, () => {
